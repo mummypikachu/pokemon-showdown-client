741982120f92be1f56f39e0f35a917576a685e92
mummypikachu <83767883+mummypikachu@users.noreply.github.com>
2023-03-18 21:52:09 -0400

    s

--- config/config.js
+++ config/config.js
@@ -34,7 +34,7 @@ Config.customcolors = {
 	'zarel': 'aeo'
 };
 /*** Begin automatically generated configuration ***/
-Config.version = "0.11.2 (0232351e)";
+Config.version = "0.11.2 (fd0e86c0)";
 
 Config.routes = {
 	root: 'http://127.0.0.1:8000',

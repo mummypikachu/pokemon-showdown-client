f6e55e61d1843897fd9109ac502e3e5d405a67c6
mummypikachu <83767883+mummypikachu@users.noreply.github.com>
2023-03-25 19:47:04 -0400

    ads

--- config/config.js
+++ config/config.js
@@ -34,11 +34,11 @@ Config.customcolors = {
 	'zarel': 'aeo'
 };
 /*** Begin automatically generated configuration ***/
-Config.version = "0.11.2 (ce5fd2bc)";
+Config.version = "0.11.2 (334795f5)";
 
 Config.routes = {
 	root: 'pokemonshowdown.com',
-	client: 'https://192.168.1.66:8080',
+	client: '192.168.1.66',
 	dex: 'dex.pokemonshowdown.com',
 	replays: 'replay.pokemonshowdown.com',
 	users: 'pokemonshowdown.com/users',

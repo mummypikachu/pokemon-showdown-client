71c2a330bf1ae323a1ba9b5c31d169212857f88e
mummypikachu <83767883+mummypikachu@users.noreply.github.com>
2023-04-06 16:28:36 -0400

    Update config.js

--- config/config.js
+++ config/config.js
@@ -34,7 +34,7 @@ Config.customcolors = {
 	'zarel': 'aeo'
 };
 /*** Begin automatically generated configuration ***/
-Config.version = "0.11.2 (7606a293)";
+Config.version = "0.11.2 (c1b85659)";
 
 Config.routes = {
 	root: 'pokemonshowdown.com',

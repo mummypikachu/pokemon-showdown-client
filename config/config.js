b31079495b87f8372a98ec844f155b8bac7e5cde
mummypikachu <83767883+mummypikachu@users.noreply.github.com>
2023-04-16 17:38:32 -0400

    Revert "login server no need"
    This reverts commit 0ff113888df33133833357846bbf34a7316dc1d7.

--- config/config.js
+++ config/config.js
@@ -34,7 +34,7 @@ Config.customcolors = {
 	'zarel': 'aeo'
 };
 /*** Begin automatically generated configuration ***/
-Config.version = "0.11.2 (2916b590)";
+Config.version = "0.11.2 (f362c3c8)";
 
 Config.routes = {
 	root: 'pokemonshowdown.com',

c5ca3cf8c0af9bf0df33e456df209ee1ad7732d4
mummypikachu <83767883+mummypikachu@users.noreply.github.com>
2023-06-01 14:55:40 -0400

    rayqua

--- config/config.js
+++ config/config.js
@@ -32,7 +32,7 @@ Config.roomsFirstOpenScript = function () {
 
 
 /*** Begin automatically generated configuration ***/
-Config.version = "0.11.2 (c4f402e3)";
+Config.version = "0.11.2 (9f0d8b45)";
 
 Config.routes = {
 	root: 'pokemonshowdown.com',

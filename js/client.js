12f48fff37e0e569d28f89dcc35c241fa0f902e3
mummypikachu <83767883+mummypikachu@users.noreply.github.com>
2023-06-03 17:29:04 -0400

    Update client.js

--- js/client.js
+++ js/client.js
@@ -216,7 +216,7 @@ function toId() {
 		 * domain in order to have access to the correct cookies.
 		 */
 		getActionPHP: function () {
-			var ret = '/~~' + Config.server.id + '/action.php';
+			var ret = '/action.php';
 			if (Config.testclient) {
 				ret = 'http://' + Config.routes.client + ret;
 				;

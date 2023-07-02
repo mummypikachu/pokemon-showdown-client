f5976268f5d6ad62c3fe8406b74c15cc421ae998
mummypikachu <83767883+mummypikachu@users.noreply.github.com>
2023-07-01 21:00:47 -0400

    sss

--- config/config.js
+++ config/config.js
@@ -32,7 +32,7 @@ Config.roomsFirstOpenScript = function () {
 
 
 /*** Begin automatically generated configuration ***/
-Config.version = "0.11.2 (cda6c3f8)";
+Config.version = "0.11.2 (71aff388)";
 
 Config.routes = {
 	root: 'pokemonshowdown.com',

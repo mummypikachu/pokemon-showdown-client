7df931e746a0f2922c7cc12f9b305aba97f79990
mummypikachu <83767883+mummypikachu@users.noreply.github.com>
2023-08-25 16:57:48 -0400

    w showdown moment

--- config/config.js
+++ config/config.js
@@ -32,7 +32,7 @@ Config.roomsFirstOpenScript = function () {
 
 
 /*** Begin automatically generated configuration ***/
-Config.version = "0.11.2 (01c1518e/6e393be7)";
+Config.version = "0.11.2 (743fa8fd)";
 
 Config.routes = {
 	root: 'pokemonshowdown.com',
